41efc2acf653fda383d243c71448c6df6bec8ff7
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-10-30 23:30:05 -0300

    initialize

---
+++ Uqs.AppointmentBooking.Domain.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Uqs.AppointmentBooking.Domain.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Uqs.AppointmentBooking.Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Uqs.AppointmentBooking.Domain;
+public class Class1
+{
+
+}
