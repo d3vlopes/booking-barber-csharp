1f8acd885c0e67e0abed2ec5d76d39d78fac0041
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-10-30 23:38:57 -0300

    feat: add SeedData

--- Uqs.AppointmentBooking.WebApi/Program.cs
+++ Uqs.AppointmentBooking.WebApi/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Uqs.AppointmentBooking.Domain.Database;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppointmentBooking")));
 
 // Add services to the container.
 
