f46a068bf89215a987e18046e1a314782ec19770
Leandro Lopes <leandro-lopes46@hotmail.com>
2023-10-31 01:20:00 -0300

    feat: add SlotsService

---
+++ Uqs.AppointmentBooking.Domain.Tests.Unit/SlotsServiceTests.cs
@@ -0,0 +1,67 @@
+﻿
+
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using Xunit;
+
+using Uqs.AppointmentBooking.Domain.Services;
+using Uqs.AppointmentBooking.Domain.Tests.Unit.Fakes;
+
+
+
+namespace Uqs.AppointmentBooking.Domain.Tests.Unit
+{
+    public class SlotsServiceTests : IDisposable
+    {
+        private readonly ApplicationContextFakeBuilder _contextBuilder = new();
+        private SlotsService? _sut;
+        private readonly INowService _nowService = Substitute.For<INowService>();
+        private readonly IOptions<ApplicationSettings> _settings =
+       Substitute.For<IOptions<ApplicationSettings>>();
+
+
+        public void Dispose()
+        {
+            _contextBuilder.Dispose();
+        }
+
+        [Fact]
+        public async Task GetAvailableSlotsForEmployee_ServiceIdNoFound_ArgumentException()
+        {
+            // Arrange
+            var context = _contextBuilder.Build();
+
+            _sut = new SlotsService(context, _nowService, _settings);
+
+            // Act
+            var exception = await Assert.ThrowsAsync<ArgumentException>(() => _sut.GetAvailableSlotsForEmployee(-1, -1));
+
+            // Assert
+            Assert.IsType<ArgumentException>(exception);
+        }
+
+        [Fact]
+        public async void GetAvailableSlotsForEmployee_NoShiftsForTomAndNoAppointmentsInSystem_NoSlots()
+        {
+            // Arrange
+            var appointmentFrom =
+            new DateTime(2022, 10, 3, 7, 0, 0);
+            _nowService.Now.Returns(appointmentFrom);
+            var context = _contextBuilder
+            .WithSingleService(30)
+            .WithSingleEmployeeTom()
+            .Build();
+            _sut = new SlotsService(context, _nowService, _settings);
+            var tom = context.Employees!.Single();
+            var mensCut30Min = context.Services!.Single();
+            // Act
+            var slots = await
+            _sut.GetAvailableSlotsForEmployee(
+            mensCut30Min.Id, tom.Id);
+            // Assert
+            var times = slots.DaysSlots.SelectMany(x => x.Times);
+            Assert.Empty(times);
+
+        }
+    }
+}
---
+++ Uqs.AppointmentBooking.Domain/Services/SlotsService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+using Uqs.AppointmentBooking.Domain.DomainObjects;
+using Uqs.AppointmentBooking.Domain.Database;
+using Uqs.AppointmentBooking.Domain.Report;
+
+namespace Uqs.AppointmentBooking.Domain.Services
+{
+    public class SlotsService
+    {
+        private readonly ApplicationContext _context;
+        private readonly ApplicationSettings _settings;
+        private readonly DateTime _now;
+        private readonly TimeSpan _roundingIntervalSpan;
+
+        public SlotsService(ApplicationContext context, INowService nowService, IOptions<ApplicationSettings> settings)
+        {
+            _context = context;
+            _settings = settings.Value;
+            _now = RoundUpToNearest(nowService.Now);
+
+        }
+
+        public async Task<Slots> GetAvailableSlotsForEmployee(int serviceId, int employeeId)
+        {
+            var service = await _context.Services!.SingleOrDefaultAsync(x => x.Id == serviceId);
+
+            if (service is null)
+            {
+                throw new ArgumentException("Record not found", nameof(serviceId));
+            }
+
+            var shifts = _context.Shifts!.Where(x => x.EmployeeId == employeeId);
+
+            if (!shifts.Any())
+            {
+                return new Slots(Array.Empty<DaySlots>());
+            }
+
+            return null;
+        }
+
+        private DateTime RoundUpToNearest(DateTime dt)
+        {
+            var ticksInSpan = _roundingIntervalSpan.Ticks;
+            return new DateTime((dt.Ticks + ticksInSpan - 1)
+                / ticksInSpan * ticksInSpan, dt.Kind);
+        }
+    }
+}
